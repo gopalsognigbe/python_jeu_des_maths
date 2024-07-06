c009e16f34be173a8ed04500fa0ab6360677053d
Gopal <gopalsognigbe@gmail.com>
2024-07-06 13:50:58 +0100

    Update main.py
    first commit

--- main.py
+++ main.py
@@ -1,54 +1,50 @@
-def afficher_information_personne(nom,age,taille=0):
-    print(f"votre nom est {nom}, vous avez {age} ans l'an prochain vous aurez {age + 1} ans")
-    if age == 1 or age ==2:
-        print('vous etes un bebe')
-    elif age < 10 :
-        print("vous etes mineur")
-    elif age == 17:
-        print('Vous etes presque majeur')
-    elif age >= 12 and age < 18:
-        print('vous etes adolescent')
-    elif age == 18:
-        print('vous etes tout juste majeur')
-    elif age > 60:
-        print('vous etes senior')
-    elif age > 18:
-        print('vous etes majeur')
-    # afficher la taille 
-    if taille != 0:
-        print('votre taille est :'+str(taille)+' m')
+import random
+NOMBRE_MIN=1
+NOMBRE_MAX=10
+NOMBRE_Q=10
+nb_point=0
+
+trouve=False
 
 
-def demander_nom ():
-    nom= input('entrer le nom: ')
-    while nom=='':
-        nom= input('entrer le nom: ')
-    return nom
+def poser_question():
+    a=random.randint(NOMBRE_MIN,NOMBRE_MAX)
+    b=random.randint(NOMBRE_MIN,NOMBRE_MAX)
+    op=random.randint(0,1)
+    if op == 1:
+        op='+'
+        calcul=a+b
+    else:
+        op='*'
+        calcul=a*b
+    rep_str=input(f'Calculez {a} {op} {b} =')
+    rep_int=int(rep_str)
+
+    if rep_int == calcul:
+        print('Bravo reponse correct!!')
+        global trouve
+        trouve=True
+
+    else :
+        print('Dommage')
+        trouve=False
+    return trouve
 
 
-def demander_age(nom_personne):
-    age_int=0
-    while     age_int==0:
-        age=input(f'votre age {nom_personne} :')
-        try:
-            age_int= int(age)
-        except:
-            print('Erreur:vous devez rentrer un nombre ')
-    return age_int
-""" 
-#demander nom
-nom=demander_nom()
-nom1=demander_nom()
 
-#demander age
-age=demander_age(nom)
-age1=demander_age(nom1)
+for i in range (1,NOMBRE_Q+1):
+    print(f"Question {i} sur {NOMBRE_Q}:")
+    poser_question()
+    if trouve==True:
+        nb_point+=1
+    print('')
+print(f"votre resultat {nb_point}/{NOMBRE_Q}")
 
-#afficher information
-afficher_information_personne(nom,age)1
-afficher_information_personne(nom1,age1)
-"""
-for i in range(0,3):
-    nom='personne '+ str(i +1)
-    age=demander_age(nom)
-    afficher_information_personne(nom,age,)
+if nb_point == 4 :
+    print('Vous etes le meilleur!!')
+elif nb_point == 0:
+    print('Tres null!!')
+elif nb_point > int(NOMBRE_Q/2): 
+    print('Peu mieux faire')
+else : 
+    print('revise tes maths ')
